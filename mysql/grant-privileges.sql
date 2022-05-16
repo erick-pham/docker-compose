08cee128eb61f425ce2077f724197877a0452810
Toan Pham <pqtoan220496@gmail.com>
2022-05-16 09:57:28 +0700

    update

--- mysql/grant-privileges.sql
+++ mysql/grant-privileges.sql
@@ -1,4 +1,2 @@
 -- login by root user to run the below scripts --
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Palomino1!';
-ALTER USER 'foresightpro'@'%' IDENTIFIED WITH mysql_native_password BY 'foresightpro_dev';
-ALTER USER 'crmdev'@'%' IDENTIFIED WITH mysql_native_password BY 'crmdev';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
